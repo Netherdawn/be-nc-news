2eab55a9fa279d019e67c7a24d70511eb1fcb1a2
Brork <jamessamueljohnson@gmail.com>
2020-02-24 12:44:36 +0000

    completed format dates util with TDD testing

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,14 @@
 exports.formatDates = list => {
-  return [];
+  if (list.length === 0) {
+    return [];
+  } else {
+    list.forEach(item => {
+      let convertedDate = new Date(item.created_at);
+      convertedDate = convertedDate.toUTCString();
+      item.created_at = convertedDate;
+    });
+    return list;
+  }
 };
 
 exports.makeRefObj = list => {};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -7,7 +7,87 @@ const {
 
 describe.only("formatDates", () => {
   it("If passed an empty array, will return an empty array", () => {
-    expect(formatDates()).to.eql([]);
+    expect(formatDates([])).to.eql([]);
+  });
+  it("If passed a simple array with only unix date, will return simple array with date swapped for valid string date", () => {
+    const input = [{ created_at: 1542284514171 }];
+    expect(formatDates(input)).to.eql([
+      { created_at: "Thu, 15 Nov 2018 12:21:54 GMT" }
+    ]);
+  });
+  it("If passed a simple array with full data set incling unix date, will return simple array with date swapped for valid string date and additional info", () => {
+    const input = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    expect(formatDates(input)).to.eql([
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: "Thu, 15 Nov 2018 12:21:54 GMT",
+        votes: 100
+      }
+    ]);
+  });
+  it("If passed a long array with full data set including unix date, will return long array with date swapped for valid string date and additional info", () => {
+    const input = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      },
+      {
+        title: "Sony Vaio; or, The Laptop",
+        topic: "mitch",
+        author: "icellusedkars",
+        body:
+          "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+        created_at: 1416140514171
+      },
+      {
+        title: "Eight pug gifs that remind me of mitch",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "some gifs",
+        created_at: 1289996514171
+      }
+    ];
+    expect(formatDates(input)).to.eql([
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: "Thu, 15 Nov 2018 12:21:54 GMT",
+        votes: 100
+      },
+      {
+        title: "Sony Vaio; or, The Laptop",
+        topic: "mitch",
+        author: "icellusedkars",
+        body:
+          "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+        created_at: "Sun, 16 Nov 2014 12:21:54 GMT"
+      },
+      {
+        title: "Eight pug gifs that remind me of mitch",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "some gifs",
+        created_at: "Wed, 17 Nov 2010 12:21:54 GMT"
+      }
+    ]);
   });
 });
 
