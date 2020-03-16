f2a0ab9a09f5dc156653d6783c3cce411dd4aa87
Brork <jamessamueljohnson@gmail.com>
2020-03-16 14:56:03 +0000

    messing around with cors dependancies to get to work

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { customErrors, psqlErrors, handle500s } = require("./errors");
 const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
@@ -12,7 +13,5 @@ app.use("/api", apiRouter);
 app.use(customErrors);
 app.use(psqlErrors);
 app.use(handle500s);
-
-app.use(cors());
 
 module.exports = app;
